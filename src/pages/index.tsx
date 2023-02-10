49701a676200cfa0d50f060e86792a2b6d97621c
James Kerr <kerr@hey.com>
2023-02-09 17:07:35 -0800

    Style the Zui Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,11 +17,10 @@ export default function Home() {
       <Head>
         <html className={styles.html} />
       </Head>
-
       <div className={styles.gridTexture}>
         <div className={styles.radialGradient}></div>
-        <div className={clsx(styles.gridContent, styles.wrap)}>
-          <div className={styles.heroContainer}>
+        <div className={styles.gridContent}>
+          <div className={clsx(styles.heroContainer, styles.wrap)}>
             <div className={styles.heroLeftSide}>
               <h1>
                 <span style={{ visibility: "hidden", position: "absolute" }}>
@@ -34,20 +33,53 @@ export default function Home() {
 
             <div className={styles.heroRightSide}>
               <img src={iconUrl} />
-              <button className={styles.primaryButton}>Download</button>
+              <a
+                href="https://brimdata.io/download"
+                className={styles.primaryButton}
+              >
+                Download
+              </a>
               <p className={clsx(styles.subText, styles.versionNumber)}>
                 v1.0.0
               </p>
             </div>
           </div>
-          <div>
-            <img src={screenshotUrl} />
+
+          <div className={styles.wrap}>
+            <h3 className={styles.tagline}>
+              A desktop app for{" "}
+              <em>
+                working with <b>data</b>
+              </em>
+            </h3>
+          </div>
+
+          <div className={styles.screenShot}>
+            <div className={styles.wrap}>
+              <div className={styles.dotTextureTopLeft}></div>
+              <div className={styles.dotTextureBottomRight}></div>
+              <img
+                src={screenshotUrl}
+                alt="Zui screenshot showing github pull request data being explored."
+              />
+            </div>
           </div>
         </div>
       </div>
+      <div className={clsx(styles.taglineContainer, styles.wrap)}>
+        <p className={styles.boxedSubtitle}>
+          Powered By <b>ZED</b>
+        </p>
+      </div>
 
-      <div className={styles.dotTexture}></div>
-      <div className={styles.dotTextureAlternate}></div>
+      <div className={styles.features}>
+        <ul>
+          <li>Drag-And-Drop Ingest</li>
+          <li>Schema Inferance</li>
+          <li>Named Queries</li>
+          <li>Filters And Pivots</li>
+        </ul>
+      </div>
     </Layout>
   );
 }
